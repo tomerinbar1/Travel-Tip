1b80d530682f5d8935d908498e49b35adcf6beff
Tomer Inbar <tomerinbar1@gmail.com>
2023-05-03 15:49:24 +0300

    add API key to google maps

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -39,7 +39,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyC5npTxFk7UL7btXdc70lyEfjwH8Mhet5g' //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
